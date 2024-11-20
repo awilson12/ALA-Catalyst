5b5eb64210d72f1657f4430ed4a800c64bc2b5df
awilson12 <apfeifer@email.arizona.edu>
2024-11-20 13:49:26 -0700

    Adding text to welcome page

--- App-1/app.R
+++ App-1/app.R
@@ -24,7 +24,7 @@ body <- dashboardBody(
   tabItems(
     tabItem(tabName = "welcome",
             h2("Welcome!"),
-            p("Welcome to the school health tool. This tool is a part of a larger resource for school health professionals, called ____________. The following tabs
+            p("Welcome to the school health tool. The following tabs
               will provide information about the tool, how to use it, and access to the tool itself. A summary of the project at large can be seen in a digital
               flyer below."),
             img(src="infosheet.png",width=900)
@@ -36,64 +36,83 @@ body <- dashboardBody(
               the spread of respiratory viral diseases, such as flu or rhinovirus, the virus responsible for
               the common cold. This application is also useful for educational purposes by demonstrating in
               real time how single or bundled interventions can reduce risks for students and teachers."),
+            p("This tool cannot connect to attendance data systems and is not for contact tracing or detecting
+              outbreaks. Rather, it is for exploring hypothetical scenarios and seeing, quantitatively, how single or
+              bundled interventions could reduce disease burden and maintain infection risk levels below 10%. In other words,
+              the tool is designed for planning and prevention. A paper describing the tool in full detail is under review in a
+              peer reviewed journal. A link to that paper will be made available here upon publication."),
             p("The development of this tool is funded by the American Lung Association, and Drs. Amanda Wilson and Ashley Lowe
               are working with Arizona-based public, private, and charter schools to inform and improve the tool's develpoment so that it is as
               accurate as possible and useful to school health professionals in making decisions regarding controlling the spread of
-              respiratory viral disease in school settings. Questions about the tool or the project can be directed to Dr. Amanda 
+              respiratory viral disease in school settings. This tool and the assumptions in developing it are not reflective of official
+              views of the American Lung Associaton and are soley the responsiblity of the research investigators.
+              Questions about the tool or the project can be directed to Dr. Amanda 
               Wilson at amwilson2@arizona.edu.")
     ),
     tabItem(tabName="How",
             h2("How to Use the Tool"),
               p("This calculator tool estimates the average infection risk per student and for the teacher for a simulation of a 1-hr class. The 
                 output is given in the form of a number of infections expected per 1,000 students or teachers. These numbers update as you change
-                settings in the model with the menu below the figure.
-                The available parameters and their descriptions are provided below. For more detailed information on model assumptions and
-                sources, please click here. (link to be entered!!!)"),
+                settings in the model with the menu below the figure. Not every student who gets infected will become ill (i.e., have symptoms), but those who are infectious
+                can still infect others without symptoms.
+                The available parameters and their descriptions are provided below."),
               p("We use a framework called 'Quantitative Microbial Risk Assessment' (QMRA), in which we use data from published scientific literature and assumptions
                 based on the inputs that to select to quantify an infection risk for a given scenario. More information on what QMRA is and how it's used can be found below."),
               tags$a(href="https://qmrawiki.org/about", "What is QMRA?"),
-              h3("Number of Students"),
-                p("This setting allows you to select the number of students in the classroom."),
-              h3("% Infected Students"),
-                p("This allows you to select what percent of the students are assumed to be infected (asymptomatic or not)."),
-              h3("Student Grade Level"),
-                p("This setting allows you to choose kindergarten through 5th grade. This input is used to estimate the average
-                  age of a child and the volume of air they would inhale over 1-hr in a classroom."),
-              h3("Teacher Age"),
-                p("This allows you to select the assumed teacher's age so that the model can estimate the volume of air that a teacher
-                  would inhale over a 1-hr class. The model considers differences in inhalation rates not only by age but also by gender and additionally
-                  incorporates the average proportions of male vs. female teachers in K-5 classrooms across the U.S."),
-              h3("Pathogen"),
+            h2("Illness-related Settings"),  
+              h3("Illness Type"),
                 p("We included pathogens for which there is published information about how the amount of virus someone inhales/ingests relates to 
                   risk of infection. This is called a 'dose response' relationship, where a curve informed by data estimates an infection risk based on
                   an estimate of how much virus enters a student's or teacher's body through the lungs or through hand-to-face contacts. To learn more
                   about dose response and its use in quantitative microbial risk assessment, please use the link below."),
-                  tags$a(href="https://qmrawiki.org/content/dose-response-assessment","Dose Response Information"),
-              h3("Air Exchange Rate"),
+            tags$a(href="https://qmrawiki.org/content/dose-response-assessment","Dose Response Information"),
+              h3("% of Students Infected"),
+               p("This allows you to select what percent of the students are assumed to be infected (asymptomatic or not). You may have a number to inform this setting depending on the number of students who are absent due to illnes, for example.
+                  Or, you  may choose a value just to see how it impacts the estimated infection risk."),
+            h2("Classrom-related Settings"),
+              h3("Number of Students"),
+                p("This setting allows you to select the number of students in the classroom."),
+              h3("Grade Level"),
+                p("This setting allows you to choose kindergarten through 5th grade. This input is used to estimate the average
+                  age of a child and the volume of air they would inhale over time."),
+              h3("Class Type"),
+                p("You  may be interested in exploring risks for general education vs.
+                  physical education or music class. This setting influences assumptions about
+                  the activity level of students and their respiration rates. For PE, it also
+                  affects assumption about the volume of the room."),
+              h3("Classroom Size"),
+                p("You can select general classroom sizes informed by the amount of space required or recommended per student and common
+                  heights in U.S. classrooms."),
+              h3("Advanced Classroom Options"),
+                p("This is a special setting if you want to explore results for a specific classroom volume."),
+            h2("Air Quality Settings"),
+              p("The basic air quality setting allows you to select from 'Poor' to 'Great,' based on
+              ranges of recommended fresh air exchange values in classrooms and reported air exchange
+              values in real-world settings. For more specific air quality settings, see 'Advanced
+              Air Quality Settings.'"),
+              h2("Advanced Air Quality Settings"),
                 p("Air exchange rate is a measure of how quickly the air in a space is replaced with fresh air. A higher air exchange rate means more
-                  fresh air replaces 'old' air over a shorter period of time. For more information on air exchange rates and recommended air exchange
+                  fresh air replaces 'old' air over a shorter period of time. Some tradeoffs to consider include increased cost of cooling or heating air
+                  if more fresh air is being circulated. For more information on air exchange rates and recommended air exchange
                   rates for different spaces, please see this information from the American Society of Heating, Refrigerating, and Air Conditioning
                   Engineers (ASHRAE)"),
                    tags$a(href="chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://www.ashrae.org/file%20library/technical%20resources/free%20resources/design-guidance-for-education-facilities.pdf", 
                          "ASHRAE information on Air Changes for Classrooms"),
-              h3("Portable HEPA Filter"),
-                p("This allows you to select whether a portable HEPA filter is assumed to be on in the classroom and working effectively. Portable
-                  HEPA filters can be very useful in reducing not only the amount of virus in the air but also dust that can exacerbate students
-                  with asthma."),
-              h3("Open Windows & Doors"),
-                p("These options allow you to select whether to assume windows and/or doors are open or closed in the classroom. Open windows and doors can increase the amount of
-                  fresh air that is circulated in the room, diluting the amount of virus that is exhaled by infected students. We assume an average size
-                  of _______ per window and ________ windows and an average size of ______ per door and 1 door to the classroom. While the amount of fresh air can depend greatly 
-                  on the weather and whether the classroom door exits to outdoors or indoors, we assume _________."),
-              h3("Classroom Volume"),
-                p("You can select the assumed volume of the classroom. This is used in the model to estimate how virus will accumulate in the air, given other
-                  assumptions, such as the air exchange rate, percent of sick students, etc."),
-              h3("Student and Teacher Masks"),
-                p("In these settings, you can select the percent of students that are wearing masks (including 0% as an option) and whether the
-                  teacher is wearing a maks. Because the effectiveness of masks depends upon the material type, you can also select the type of mask
-                  being used. This is assumed to be used by the teacher and the students, if any students are assumed to be wearing masks. The mask effectiveness
+                p("The portable air purifier option allows you to select whether one of these devices is assumed to be on in the classroom and working effectively. These devices can be very useful in reducing not only the amount of virus in the air but also dust that can exacerbate students
+                  with asthma. Tradeoffs to consider include maintenance (changing out of filters) and being able to safely plug it into an outlet. Some
+                  may be noisy, and the amount of air the device filters per time should be considered when purshasing one or more for a given space."),
+                p("You can also decide whether to assume windows and/or doors are open or closed in the classroom. Open windows and doors can increase the amount of
+                  fresh air that is circulated in the room, diluting the amount of virus that is exhaled by infected students. We assume that opening doors or
+                  windows will double the current fresh air exchange. The pros of this approach is it is inexpensive since it doesn't require new equipment.
+                  However, the tradeoffs to consider include potential distractions for students, safety concerns, weather, or sources of air pollution
+                  that could be nearby the school, such as for schools in high vehicle traffic areas."),
+              h3("Percent of Student Masks"),
+                p("In this setting, you can select the percent of students that are wearing masks (including 0% as an option). The mask effectiveness
                   values used in the model assume that the mask is being worn propertly (i.e., sealed securly to the chin and nose). So reductions in risk
-                  that are estimated are optimistic, and barriers to feasibility in proper use should be considered.")
+                  that are estimated are optimistic, and barriers to feasibility in proper use should be considered. Tradeoffs to consider include
+                  safety issues, such as choking hazards with the straps, for small students or students with special educational needs and the
+                  abilitiy to enforce compliance with wearing them properly. It may also interfere with certain subjects, such as phonics or
+                  English as a Second Language (ESL) education.")
             ),
     tabItem(tabName= "calc",
             #h2("Click through choices and see changes in risk in real time"),
